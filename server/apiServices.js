b58a35bdab8e1c04f98673320cd52b87bfad3e14
Anastasia Agustine <Anastasia.Agustine@kst.co.id>
2023-05-08 03:52:01 +0700

    server: simplified api service

--- server/apiServices.js
+++ server/apiServices.js
@@ -2,6 +2,7 @@ const utils = require("./utils.js");
 
 const chatGPTSummarizer = require("./Summarizer_OpenAi.js");
 const tfIdfSummarizer = require("./Summarizer_TfIdf.js");
+const generateRouge = require("./Generate_Rouge.js")
 
 module.exports.summaryService = async function(req, res) {
   console.log(">> Summary Service");
@@ -9,25 +10,25 @@ module.exports.summaryService = async function(req, res) {
   let body = utils.getBody(req);
   let sentenceAllBody = body.textDocument;
 
-  const summary = tfIdfSummarizer.summarizeText(sentenceAllBody);
+  let result = {};
+
+  if (sentenceAllBody === "") {
+    let error = "no text detected";
+    result.error = error;
+
+    return result;
+  }
+
+  const generatedSummary = tfIdfSummarizer.summarizeText(sentenceAllBody);
   const referenceSummary = await chatGPTSummarizer.generateSummary(sentenceAllBody);
 
-  // const accuracy = evaluateSummarizer(summary, );
+  const rouge = generateRouge.generateRouge(generatedSummary, referenceSummary);
 
-  const result = {
-    score: scores,
-    summary: summary,
-    referenceSummary: referenceSummary
+  result = {
+    summary: generatedSummary,
+    rouge: rouge
   };
 
+  console.log(">> End Service");
   return result;
 }
-
-// function evaluateSummarizer(summary, referenceSummary) {
-//   // calculate precision and recall using the Rouge metric
-//   const scores = rouge(summary, referenceSummary);
-//   const precision = scores["rouge-1"]["p"];
-//   const recall = scores["rouge-1"]["r"];
-
-//   return { precision, recall };
-// }
