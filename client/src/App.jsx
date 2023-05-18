0261889fb92e2e7aef39c0ef10dedd3f600f768c
Anastasia Agustine <Anastasia.Agustine@kst.co.id>
2023-05-18 14:50:04 +0700

    client: add css for app, update handling for accuracy component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react';
 import { Stack, Typography } from '@mui/material';
 import AccuracyComponent from "./components/AccuracyComponent.jsx";
 import TextArea from "./components/TextAreaComponent.jsx";
+import "./styles.css";
 
 function App() {
   const [isLoading, setIsLoading] = useState(0);
-  const [accuracy, setAccuracy] = useState({});
+  const [accuracy, setAccuracy] = useState({
+    Precision: 0,
+    Recall: 0,
+    F1Score: 0
+  });
 
   return (
-    <Stack className="App">
-      <Stack my={1}>
-        <Typography variant='h4'>Base React Project</Typography>
+    <Stack className="App" justifyContent="center" alignItems="center">
+      <Stack mb={2} alignItems="center">
+        <Typography variant='h4'>Final Project:</Typography>
         <Typography variant='h5'>Extractive Text Summarizer</Typography>
       </Stack>
       <TextArea setIsLoading={setIsLoading} setAccuracy={setAccuracy} />
--- client/src/components/AccuracyComponent.jsx
+++ client/src/components/AccuracyComponent.jsx
@@ -1,15 +1,15 @@
 import {Stack, Typography } from '@mui/material';
 
 const AccuracyComponent = ({ accuracy }) => {
-  let precision = accuracy.Precision;
-  let recall = accuracy.Recall;
-  let f1 = accuracy.F1Score;
+  let precision = accuracy.Precision? accuracy.Precision: null;
+  let recall = accuracy.Recall? accuracy.Recall : null;
+  let f1 = accuracy.F1Score? accuracy.F1Score: null;
 
   return (
-    <Stack>
-      <Typography>Precision: {precision}</Typography>
-      <Typography>Recall: {recall}</Typography>
-      <Typography>F1 Score: {f1}</Typography>
+    <Stack display={'block'} width={'72%'}>
+      <Typography className='label'>Precision: {precision}</Typography>
+      <Typography className='label'>Recall: {recall}</Typography>
+      <Typography className='label'>F1 Score: {f1}</Typography>
     </Stack>
   );
 };
